663ed284f4c055ed331a94600d1ac6ad33453c6f
ecyrbe <ecyrbe@gmail.com>
2022-07-23 18:23:59 +0200

    fix: plugin config should be immutable

--- src/api.ts
+++ src/api.ts
@@ -1,27 +1,26 @@
-import { AxiosRequestConfig } from "axios";
-import { ZodiosInstance, ZodiosPlugin } from "@zodios/core";
-import type { ZodiosEnpointDescriptions } from "@zodios/core";
+import { ZodiosPlugin } from "@zodios/core";
 
 const plugin: ZodiosPlugin = {
   name: "api",
   request: async (api, config) => {
-    // istanbul ignore next
-    if (!config.headers) {
-      config.headers = {};
-    }
     if (config.method !== "get") {
-      config.headers = {
-        ...config.headers,
-        "Content-Type": "application/json",
-        Accept: "application/json",
+      return {
+        ...config,
+        headers: {
+          ...config.headers,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
       };
     } else {
-      config.headers = {
-        ...config.headers,
-        Accept: "application/json",
+      return {
+        ...config,
+        headers: {
+          ...config.headers,
+          Accept: "application/json",
+        },
       };
     }
-    return config;
   },
 };
 
