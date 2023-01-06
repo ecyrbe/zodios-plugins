98df0ffcaecb00816ae7ae40b965ada002c78e81
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2023-01-06 11:23:27 -0300

    feat(plugins): add plugin base url

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { pluginApi } from "./api";
 export { pluginHeader } from "./header";
+export { pluginBaseURL } from "./base-url";
 export { pluginToken, TokenProvider } from "./token";
 export { pluginFetch } from "./fetch/fetch-plugin";
 export type { FetchPluginOptions } from "./fetch/fetch.types";
